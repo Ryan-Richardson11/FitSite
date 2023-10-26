6a82724e766ce7f3f3fb36fe9a1b18110b593928
Ryan Richardson <ryanrich811@gmail.com>
2023-10-26 18:42:03 -0400

    branch update

--- calculations.js
+++ calculations.js
@@ -1,15 +1,12 @@
 
-function BMI() {
-    var weight = parseFloat(prompt("Enter you weight in lbs: "));
-    var height = parseFloat(prompt("Enter your height in inches: "));
+function BMI(weight, height) {
 
     if (isNaN(weight) || isNaN(height)) {
         console.log("Please enter valid numbers for weight and height.");
         return;
     }
-
     var BMI = 703 * (weight / (height ** 2));
-    console.log("Your BMI is " + BMI.toFixed(1));
+    return window.location.href = "bmi.html?bmi=" + BMI.toFixed(1);
 }
 
 function caloricIntake() {
