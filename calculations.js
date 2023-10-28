da93eee8e815130544a5057f7ed42eadaee3af44
unknown <ryanrich811@gmail.com>
2023-10-28 14:13:20 -0400

    Created function to find caloric intake as well as HTMl page corresponding to it

--- calculations.js
+++ calculations.js
@@ -9,7 +9,40 @@ function BMI(weight, height) {
     return window.location.href = "bmi.html?bmi=" + BMI.toFixed(1);
 }
 
-function caloricIntake() {
+function caloricIntake(gender, weight, height, age, activityLevel) {
+    weight = (weight / 2.2);
+    height = (height * 2.54);
+
+    if (gender == "male") {
+        var RMR = (9.99 * weight) + (6.25 * height) - (4.92 * age) + 5;
+        if (activityLevel = "Sedentary") {
+            return RMR;
+        }
+        else if (activityLevel = "Low Active") {
+            return RMR *= 1.11
+        }
+        else if (activityLevel = "Active") {
+            return RMR *= 1.25
+        }
+        else if (activityLevel = "Very Active") {
+            return RMR *= 1.48
+        }
+    }
+    else if (gender == "female") {
+        var RMR = (9.99 * weight) + (6.25 * height) - (4.92 * age) - 161;
+        if (activityLevel = "Sedentary") {
+            return RMR;
+        }
+        else if (activityLevel = "Low Active") {
+            return RMR *= 1.12
+        }
+        else if (activityLevel = "Active") {
+            return RMR *= 1.27
+        }
+        else if (activityLevel = "Very Active") {
+            return RMR *= 1.45
+        }
+    }
 
 }
 
